41c5f17492a0b3386268219e0f09baa4c5300b53
techlearn-skh <techlearn508@gmail.com>
2025-11-16 23:21:59 +0530

    new changes

--- api-gate-way-scg/api-gate-way-scg/api-gate-way-scg/src/main/java/com/skh/ApiGateWayScgApplication.java
+++ api-gate-way-scg/api-gate-way-scg/api-gate-way-scg/src/main/java/com/skh/ApiGateWayScgApplication.java
@@ -2,8 +2,10 @@ package com.skh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGateWayScgApplication {
 
 	public static void main(String[] args) {
--- api-gate-way-scg/second-service/second-service/src/main/java/com/skh/SecondController.java
+++ api-gate-way-scg/second-service/second-service/src/main/java/com/skh/SecondController.java
@@ -2,6 +2,8 @@ package com.skh;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,13 @@ import java.time.LocalDateTime;
 @RequestMapping("/ss")
 public class SecondController {
     private static final Logger log = LoggerFactory.getLogger(SecondController.class);
+
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/message")
     public String test() {
-        String message = String.format("%s() method called in %s!!!, time %s",Thread.currentThread().getStackTrace()[1].getMethodName(), this.getClass().getName(), LocalDateTime.now());
+        String message = String.format("%s method called in %s!!!, Running on PORT: %s",Thread.currentThread().getStackTrace()[1].getMethodName(), this.getClass().getName(), environment.getProperty("server.port"));
         log.info(message);
         return message;
     }
