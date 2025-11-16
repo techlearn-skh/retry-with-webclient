240afef0b11aa7b456d43f08188c40907fd43744
kamal <skkamal2java@gmail.com>
2025-11-16 19:50:01 +0530

    new changes

---
+++ api-gate-way-scg/second-service/second-service/src/main/java/com/skh/SecondController.java
@@ -0,0 +1,21 @@
+package com.skh;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/ss")
+public class SecondController {
+    private static final Logger log = LoggerFactory.getLogger(SecondController.class);
+    @GetMapping("/message")
+    public String test() {
+        String message = String.format("%s method called in %s!!!, time %s",Thread.currentThread().getStackTrace()[1].getMethodName(), this.getClass().getName(), LocalDateTime.now());
+        log.info(message);
+        return message;
+    }
+}
